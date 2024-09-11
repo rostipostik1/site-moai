810bcd26dc3ff28d59a46df90c2b12295339528f
rostipostik <132700854+rostipostik@users.noreply.github.com>
2024-09-11 18:09:56 +0100

    commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# site-moai
+##  README.md
---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('.testimonials-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow:"<img class='prev slick-btn' src='./img/arrow-right.svg'>",
+        nextArrow:"<img class='next slick-btn' src='./img/arrow-left.svg'>",
+        asNavFor: '.slider-avatars'
+    });
+    $('.slider-avatars').slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '60px',
+        asNavFor: '.testimonials-slider'
+    });
+
+    $('.quise-item').click(function(){
+        $('.quise-item').removeClass('active')
+        $(this).addClass('active')
+
+    })
+});
